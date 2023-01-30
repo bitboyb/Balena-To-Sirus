474e332fc2c488aee5888e33d9c5f35cc11606a1
Wellpool <117166063+Wellpool@users.noreply.github.com>
2023-01-30 17:37:13 +0000

    nav mesh, bits and bobs

---
+++ Balena to Sirus/Assets/Scripts/AI/Pirate.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pirate : MonoBehaviour
+{
+    public GameObject playerLoc;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if player is in X distance
+            //move to wards x location at y speed
+            //
+                //if in z distance of player, stop and start FireAtPlayer()
+    }
+
+    void FireAtPlayer()
+    {
+        
+    }
+}
