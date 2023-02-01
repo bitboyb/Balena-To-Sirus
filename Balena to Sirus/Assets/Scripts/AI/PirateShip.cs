e896c0e8a370f1c1941ed4482e56ff9d1e84c637
Wellpool <cameronjons@hotmail.com>
2023-02-01 13:55:45 +0000

    Pirate Patrol system

--- Balena to Sirus/Assets/Scripts/AI/PirateShip.cs
+++ Balena to Sirus/Assets/Scripts/AI/PirateShip.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,27 +6,90 @@ using UnityEngine.AI;
 
 public class PirateShip : MonoBehaviour
 {
+    //View Distance of player range
     public float viewDistance = 10f;
+    public float checkPointCounter;
 
-    public GameObject firePoint;
-    
-    
-    public Transform target;
+    private bool chasingPlayer;
+
+    //For chasing the player
+    public Transform player;
+
+    public Transform checkPoint1;
+    public Transform checkPoint2;
+    public Transform checkPoint3;
+
 
     public NavMeshAgent agent;
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        chasingPlayer = false;
+        checkPointCounter = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Checks if player is in distance, then chance player if still in range.
-        if(Vector3.Distance(transform.position, target.position) < viewDistance)
-            agent.SetDestination(target.position);
+        if (chasingPlayer == false)
+            CheckPointSystem();
 
+
+        //Checks if player is in distance, then chance player if still in range.
+        if (Vector3.Distance(transform.position, player.position) < viewDistance)
+        {
+            chasingPlayer = true;
+            ChasePlayer();
+        }
+        else
+        {
+            chasingPlayer = false;
+        }
+    }
+
+    private void CheckPointSystem()
+    {
+        if (checkPointCounter == 1)
+        {
+            agent.SetDestination(checkPoint1.position);
+
+        }
+
+        if (checkPointCounter == 2)
+        {
+            agent.SetDestination(checkPoint2.position);
+
+        }
+
+        if (checkPointCounter == 3)
+        {
+            agent.SetDestination(checkPoint3.position);
+
+        }
+    }
+
+    void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CheckPoint1"))
+        {
+            checkPointCounter++;
+        }
+
+        if (other.CompareTag("CheckPoint2"))
+        {
+            checkPointCounter++;
+        }
+
+        if (other.CompareTag("CheckPoint3"))
+        {
+            checkPointCounter = 1;
+        }
     }
 }
